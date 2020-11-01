295a594613153c0514324b7e9271fa7ca31b8f92
Ivona Ze <zelinkova.ivona@gmail.com>
2020-11-01 12:20:25 +0100

    navigation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import './style.css';
 
 console.log('funguju!');
 
-// const navigation = document.querySelector('#nav-btn');
-
-// .addEventListener('click', selectSmiley);
-
-// event.target.classList.add('board__field--circle');
-
-// const navigation = (event) => {
-//   if (praveHraje === 'circle') {
-//     event.target.classList.add('board__field--circle');
+const navigace = document.querySelector('#nav-btn');
+navigace.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
