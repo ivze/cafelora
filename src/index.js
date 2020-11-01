9977711b34906c4c186df046a718b3d1beda8e78
Ivona Ze <zelinkova.ivona@gmail.com>
2020-11-01 11:30:32 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+// const navigation = document.querySelector('#nav-btn');
+
+// .addEventListener('click', selectSmiley);
+
+// event.target.classList.add('board__field--circle');
+
+// const navigation = (event) => {
+//   if (praveHraje === 'circle') {
+//     event.target.classList.add('board__field--circle');
